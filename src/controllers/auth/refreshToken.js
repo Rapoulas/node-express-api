dfc7cffa8ca9b498727ec18b9ad0c4c82a975296
Rafael <141091884+Rapoulas@users.noreply.github.com>
2024-03-30 11:56:07 -0300

    Update refreshToken.js

--- src/controllers/auth/refreshToken.js
+++ src/controllers/auth/refreshToken.js
@@ -4,40 +4,63 @@ import userModel from '../../models/userModel.js'
 import { prisma } from '@prisma/client'
 
 const refreshToken = (req, res) => {
-    let token = false
-    //TODO: obter cookie refresh token
-    const authorization = req.headers?.authorization
-    if (authorization) token = authorization.split(' ')[1]
+  // TODO: Obter cookie refreshToken
+  const authorization = req.headers.authorization?.split(' ')[1]
 
-    if (!token) return res.status(401).json({
-        error: 'Usuário não autorizado',
-        message: error.message,
-        code: 'token-not-found'
+  if (!authorization) return res.status(401).json({
+    error: "Usuario não autenticado", 
+    code: "refresh-token-not-found"
+  })
+
+  jwt.verify(token, SECRET_KEY, async (error, decoded) => {
+    if(error) return res.status(401).json({
+      error: error.message, 
+      code: "invalid-refresh-token"
     })
-    
-    jwt.verify(token, SECRET_KEY, async (error, decoded) => {
-        //TODO: verificar tipos de erros para dar a mensagem/code específica
-        if (error) return res.status(401).json({
-            error: 'Usuário não autorizado',
-            message: error.message,
-            code: 'invalid-token'
-        })
-        const userFound = await userModel.getById(decoded.id)
-        await prisma.session.findUnique()
 
-        const accessToken = jwt.sign(
-            {id: userFound.id, name: userFound.name}, // payload -> dados que você quer guardar no token
-            SECRET_KEY, // chave secreta
-            {expiresIn: '1m'} 
-            )
-        const refreshToken = jwt.sign(
-            {id: userFound.id, name: userFound.name}, // payload -> dados que você quer guardar no token
-            SECRET_KEY, // chave secreta
-            {expiresIn: '3m'} 
-        )
-
-        
+    const userFound = await userModel.getById(decoded.id)
+    const sessionFound = await prisma.session.findUnique({
+      where: {
+        token: token,
+        user_id: userFound.id
+      }
     })
+
+    if (!sessionFound) return res.status(401).json({
+      code: "session deprecaded"
+    })
+
+    const accessToken = jwt.sign(
+      {id: userFound.id, name: userFound.name}, // Payload
+      SECRET_KEY, // Secret Key to validate
+      {expiresIn: '1m', algorithm: 'HS256'} // Expire time
+    )
+    const refreshToken = jwt.sign(
+      {id: userFound.id},
+      SECRET_KEY, // Secret Key to validate
+      {expiresIn: '3m', algorithm: 'HS256'} // Expire time
+    )
+
+    await prisma.session.update({
+      where: {
+        token: token,
+        user_id: userFound.id
+      },
+      data: {
+        token: refreshToken
+      }
+    })
+
+    delete userFound.password
+    return res.json({
+      success: `Access Token e Refresh Token revalidado!`,
+      user: userFound,
+      accessToken: accessToken,
+      refreshToken: refreshToken
+    })
+  })
+
+  return true
 }
 
 export default refreshToken
